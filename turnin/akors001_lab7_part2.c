9cbd3f8210085d1e43379d22bbfe5e4565363b7c
ArmaniKorsich <33210973+ArmaniKorsich@users.noreply.github.com>
2020-05-09 01:27:47 -0700

    Update akors001_lab7_part2.c

--- turnin/akors001_lab7_part2.c
+++ turnin/akors001_lab7_part2.c
@@ -7,6 +7,9 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *	https://youtu.be/FMRWalO7YMQ
+ *	VIDEO SUBMISSION ^^^
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
